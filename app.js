323bb9ad7fa8bafe4b77ab506e780ab38dec0a6c
Lael Pierre <lael.aichaoui@gmail.com>
2022-10-20 18:40:27 -0700

    js functionnality added

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+// select btns
+const sidebarToggle = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
 
+// select class toggle
+const sidebar = document.querySelector(".sidebar");
+
+// add functionnality
+sidebarToggle.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
